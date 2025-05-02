f77dbdfd6a928618e672caf60dcb8db81cc09db6
Joaquin Rapela <joacorapela@gmail.com>
2025-05-02 10:38:42 +0100

    Added simulateNDSgaussianNoise

--- src/lds/simulation.py
+++ src/lds/simulation.py
@@ -1,18 +1,99 @@
 import numpy as np
 
-def simulateLDS(N, B, Q, Z, R, m0, V0):
+
+def simulateLDS(T, B, Q, m0, V0, Z, R):
+    """ Simulation of linear dynamical system
+
+    :param: T: number of observations
+    :type: T: int
+
+    :param: B: state transition matrix
+    :type: B: numpy matrix (MxM)
+
+    :param: Q: state noise covariance matrix
+    :type: Q: numpy matrix (MxM)
+
+    :param: m0: initial state mean
+    :type: m0: one-dimensional numpy array (M)
+
+    :param: V0: initial state covariance
+    :type: V0: numpy matrix (MxM)
+
+    :param: Z: state to observation matrix
+    :type: Z: numpy matrix (NxM)
+
+    :param: R: observations covariance matrix
+    :type: R: numpy matrix (NxN)
+
+    :return: (initial state, states, observations)
+    :rtype: tuple(numpy.array[M], numpy.array[M, T], numpy.array[P, T])
+
+    """
+
     M = B.shape[0]
     P = Z.shape[0]
     # state noise
-    w = np.random.multivariate_normal(np.zeros(M), Q, N).T
+    w = np.random.multivariate_normal(np.zeros(M), Q, T).T
     # measurement noise
-    v = np.random.multivariate_normal(np.zeros(P), R, N).T
+    v = np.random.multivariate_normal(np.zeros(P), R, T).T
     # initial state noise
-    x = np.empty(shape=(M, N))
-    y = np.empty(shape=(P, N))
+    x = np.empty(shape=(M, T))
+    y = np.empty(shape=(P, T))
     x0 = np.random.multivariate_normal(m0, V0, 1).flatten()
     x[:, 0] = B @ x0 + w[:, 0]
-    for n in range(1, N):
+    for n in range(1, T):
         x[:, n] = B @ x[:, n-1] + w[:, n]
     y = Z @ x + v
+    return x0, x, y
+
+
+def simulateNDSgaussianNoise(T, B, Q, m0, V0, Z, R):
+    """ Simulation of nonlinear dynamical system with Gaussian noise
+
+    :param: T: number of observations
+    :type: T: int
+
+    :param: B: state transition function
+    :type: B: :math:`\Re^M\rightarrow\Re^M`
+
+    :param: Q: state noise covariance function
+    :type: Q: :math:`\Re^M\rightarrow\Re^{M\times M}`
+
+    :param: m0: initial state mean
+    :type: m0: one-dimensional numpy array (M)
+
+    :param: V0: initial state covariance
+    :type: V0: numpy matrix (MxM)
+
+    :param: Z: state to observation function
+    :type: Z: :math:`\Re^M\rightarrow\Re^N`
+
+    :param: R: observations covariance function
+    :type: R: :math:`\Re^M\rightarrow\Re^{N\times N}`
+
+    :return: (initial state, states, observations)
+    :rtype: tuple(numpy.array[M], numpy.array[M, T], numpy.array[P, T])
+
+    """
+
+    M = m0.shape[0]
+    x = np.empty(shape=(M, T))
+    x0 = np.random.multivariate_normal(m0, V0, 1).flatten()
+    w = np.random.multivariate_normal(np.zeros(M), Q(x0), 1).flatten()
+    x[:, 0] = B(x0) + w
+
+    aux_cov = R(x[:, 0])
+    N = aux_cov.shape[0]
+    v = np.random.multivariate_normal(np.zeros(N), aux_cov, 1).flatten()
+    y = np.empty(shape=(N, T))
+    y[:, 0] = Z(x[:, 0]) + v
+
+    for n in range(1, T):
+        w_nM1 = np.random.multivariate_normal(np.zeros(M),
+                                              Q(x[:, n-1]), 1).flatten()
+        x[:, n] = B(x[:, n-1]) + w_nM1
+        v_n = np.random.multivariate_normal(np.zeros(N),
+                                            R(x[:, n]), 1).flatten()
+        y[:, n] = Z(x[:, n]) + v_n
+
     return x0, x, y
