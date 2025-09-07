a8e11a72f54d1dc7258f72b994a092ba90f16d1b
Joaquin Rapela <joacorapela@gmail.com>
2025-09-07 23:02:04 +0100

    Rebuilt sphik gallery

--- docs/sphinx/build/html/_downloads/9024b9988e45f9ab76641dc380167dbc/plot_MC_MAZE_SMALL.py
+++ docs/sphinx/build/html/_downloads/9024b9988e45f9ab76641dc380167dbc/plot_MC_MAZE_SMALL.py
@@ -62,7 +62,7 @@ sigma_V0 = 0.1
 
 # estimation parameters
 # max_iter = 2000
-max_iter = 5
+max_iter = 2000
 tol = 1e-1
 vars_to_estimate = {"B": True, "Q": True, "Z": True, "R": True,
                     "m0": True, "V0": True, }
